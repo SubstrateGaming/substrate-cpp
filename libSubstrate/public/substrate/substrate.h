17b501c01530d11eb58eb06756c0b9613b1738be
Sven Scharmentke <sven.scharmentke@liasoft.de>
2024-03-20 16:20:15 +0100

    Use cURL if not already present from submodule.

--- libSubstrate/public/substrate/substrate.h
+++ libSubstrate/public/substrate/substrate.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "substrate_export.h"
+
+#include <string>
+
+namespace substrate
+{
+   namespace deps
+   {
+      [[nodiscard]]
+      LIB_SUBSTRATE_EXPORT std::string get_curl_version_info();
+   }
+}
--- libSubstrate/src/substrate.cpp
+++ libSubstrate/src/substrate.cpp
@@ -1,1 +1,23 @@
 #include <substrate/substrate.h>
+
+#include <sstream>
+
+#include <curl/curl.h>
+
+std::string substrate::deps::get_curl_version_info()
+{
+   curl_version_info_data *version_info = curl_version_info(CURLVERSION_NOW);
+   std::stringstream version_details;
+
+   version_details << "cURL Version: " << version_info->version << "\n";
+   version_details << "SSL Version: " << (version_info->ssl_version ? version_info->ssl_version : "Not Available") << "\n";
+   version_details << "Libz Version: " << (version_info->libz_version ? version_info->libz_version : "Not Available") << "\n";
+   version_details << "Protocols: ";
+
+   for (const char *const *protocol = version_info->protocols; *protocol; ++protocol)
+   {
+      version_details << *protocol << " ";
+   }
+
+   return version_details.str();
+}
